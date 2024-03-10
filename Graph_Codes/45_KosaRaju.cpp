79adde5461da35a2b379c34de663892bbc133e55
akshita2703 <rastogi32akshita@gmail.com>
2024-03-10 14:27:05 +0530

    Kosaraja Algorithm

---
+++ Graph_Codes/45_KosaRaju.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    private:
+    void dfs(int node , vector<int>&vis ,vector<vector<int>>& adj,stack<int>&st){
+        vis[node]=1;
+        for(auto it : adj[node]){
+            if(!vis[it]){
+               dfs(it,vis,adj,st);
+            }
+        }
+          st.push(node);
+    }
+    private:
+    
+    void dfs2(int node , vector<int>&vis ,vector<int> adjR[]){
+        vis[node]=1;
+        for(auto it : adjR[node]){
+            if(!vis[it]){
+               dfs2(it,vis,adjR);
+            }
+        }
+        
+        
+    }
+    
+	public:
+	//Function to find number of strongly connected components in the graph.
+    int kosaraju(int V, vector<vector<int>>& adj)
+    {
+        vector<int> vis(V,0);
+        stack<int> st;
+        for(int i =0;i<V;i++){
+            if(!vis[i]){
+                dfs(i,vis,adj,st);
+            }
+        }
+        // reverse the  graph
+        
+        vector<int> adjR[V];
+        for(int i =0;i<V;i++){
+            vis[i]=0;
+            for(auto it : adj[i]){
+                // i --> it 
+                // it -->i
+                adjR[it].push_back(i);
+            }
+        }
+        int scc =0;
+        while(!st.empty()){
+            int node = st.top();
+            st.pop();
+            if(!vis[node]){
+                scc++;
+                dfs2(node, vis,adjR);
+            }
+        }
+        return scc;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+    	int V, E;
+    	cin >> V >> E;
+
+    	vector<vector<int>> adj(V);
+
+    	for(int i = 0; i < E; i++)
+    	{
+    		int u, v;
+    		cin >> u >> v;
+    		adj[u].push_back(v);
+    	}
+
+    	Solution obj;
+    	cout << obj.kosaraju(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
